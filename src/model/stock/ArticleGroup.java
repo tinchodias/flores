d14291be33df7baa30298d3b9f1e357c9c876969
tinchodias <tinchodias@99772e56-ab24-0410-9c49-6b8551dfb4e2>
2007-05-08 00:16:40 +0000

    Agrego la clase ArticleGroup. Falta implementar.

---
+++ src/model/stock/ArticleGroup.java
@@ -0,0 +1,15 @@
+package model.stock;
+
+public class ArticleGroup {
+
+	private final String name;
+
+	public ArticleGroup(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
