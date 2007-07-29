26d5615ecaad010e15824ea503455ec865ac5df0
tinchodias <tinchodias@99772e56-ab24-0410-9c49-6b8551dfb4e2>
2007-07-29 17:14:36 +0000

    Some fixes

--- trunk/src/ui/controller/initializer/search/SuppliersDialogInitializer.java
+++ trunk/src/ui/controller/initializer/search/SuppliersDialogInitializer.java
@@ -3,7 +3,7 @@ package ui.controller.initializer.search;
 import message.MessageId;
 import query.QueryFactory;
 import query.framework.query.SearchQuery;
-import ui.controller.initializer.detail.ClientDetailInitializer;
+import ui.controller.initializer.detail.SupplierDetailInitializer;
 import ui.controller.populator.SupplierPopulator;
 import ui.view.swing.component.StandardSearchDialog;
 import ui.view.swing.component.StandardSearchPanel;
@@ -12,8 +12,8 @@ import ui.view.swing.component.SuppliersSearchPanel;
 public class SuppliersDialogInitializer extends StandardSearchDialogInitializer {
 
 	protected void addActions(StandardSearchDialog searchDialog) {
-		addShowCreatingAction(new ClientDetailInitializer(), new SupplierPopulator(), searchDialog);
-		addShowModifyingAction(new ClientDetailInitializer(), new SupplierPopulator(), searchDialog);
+		addShowCreatingAction(new SupplierDetailInitializer(), new SupplierPopulator(), searchDialog);
+		addShowModifyingAction(new SupplierDetailInitializer(), new SupplierPopulator(), searchDialog);
 	}
 
 	protected StandardSearchPanel searchPanel() {
