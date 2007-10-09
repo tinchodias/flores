440544cca659c5e3edb3e17dacfc1405fcd1813c
tinchodias <tinchodias@99772e56-ab24-0410-9c49-6b8551dfb4e2>
2007-10-09 03:11:46 +0000

    DateTimePicker fix for decoration.

--- src/ui/view/swing/util/IntervalPicker.java
+++ src/ui/view/swing/util/IntervalPicker.java
@@ -13,28 +13,16 @@ import ui.view.swing.SwingUI;
 
 public class IntervalPicker extends JComponent {
 
-	private static final String DEFAULT_FORMAT = "dd/MM/yy";
 	private DateTimePicker fromDate;
 	private DateTimePicker toDate;
 
 	public IntervalPicker() {
-		setLayout(new FlowLayout());
-
 		fromDate = new DateTimePicker();
 		toDate = new DateTimePicker();
 		
+		setLayout(new FlowLayout());
 		add(SwingUI.instance().decorated(fromDate, MessageId.fromDate));
 		add(SwingUI.instance().decorated(toDate, MessageId.toDate));
-		
-		setFormat(DEFAULT_FORMAT);
-	}
-
-	/**
-	 * @see DateTimePicker#setFormat(String)
-	 */
-	private void setFormat(String simpleDateFormat) {
-		fromDate.setFormat(simpleDateFormat);
-		toDate.setFormat(simpleDateFormat);
 	}
 
 	public ReadableInterval getInterval() {
