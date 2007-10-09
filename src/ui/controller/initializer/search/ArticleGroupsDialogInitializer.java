5315a2531fb4cf31cf0e5aabaa55aed5fe74943b
tinchodias <tinchodias@99772e56-ab24-0410-9c49-6b8551dfb4e2>
2007-10-09 03:10:03 +0000

    MessageId fix.

--- src/ui/controller/initializer/search/ArticleGroupsDialogInitializer.java
+++ src/ui/controller/initializer/search/ArticleGroupsDialogInitializer.java
@@ -24,7 +24,7 @@ public class ArticleGroupsDialogInitializer extends StandardSearchDialogInitiali
 	}
 
 	public MessageId titleMessageId() {
-		return MessageId.articleGroup;
+		return MessageId.articleGroups;
 	}
 
 }
